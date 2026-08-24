17644025c0bbfaabef471191debabadae2d5e6c6
Kavindu-Madhushanka <madu10275@gmail.com>
2026-08-24 16:04:19 +0530

    change

--- backend_api/Program.cs
+++ backend_api/Program.cs
@@ -3,28 +3,38 @@ using backend_api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddDbContext<backend_api.Data.AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
-builder.Services.AddControllers();
 
-// Swagger UI 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // Swagger UI 
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowReact");
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
